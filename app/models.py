2b4e5ef513699bb1f13ed794993827f495d3f775
VladimirKhrabrov <khrabrov.v@brandsgo.digital>
2023-10-27 22:45:02 +0300

    add filefield

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class MyCustomModel(models.Model):
     my_custom_field = models.CharField(max_length=256)
+    my_custom_file_field = models.FileField(upload_to="files", null=True, blank=True)
 
     def __str__(self):
         return self.my_custom_field
